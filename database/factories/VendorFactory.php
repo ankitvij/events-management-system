14ac346c61bd6cb8e4c29ac46632c0a3ea44da3c
Ankit <ankitvijtech@gmail.com>
2026-02-12 21:43:28 +0100

    Remove faker dependency from VendorFactory

--- database/factories/VendorFactory.php
+++ database/factories/VendorFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use App\Enums\VendorType;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
-use Throwable;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Vendor>
@@ -31,25 +30,11 @@ class VendorFactory extends Factory
 
     private function generateCompanyName(): string
     {
-        try {
-            if ($this->faker !== null) {
-                return $this->faker->company();
-            }
-        } catch (Throwable) {
-        }
-
         return 'Vendor '.Str::upper(Str::random(6));
     }
 
     private function generateEmail(): string
     {
-        try {
-            if ($this->faker !== null) {
-                return $this->faker->unique()->safeEmail();
-            }
-        } catch (Throwable) {
-        }
-
         return 'vendor_'.Str::lower(Str::random(12)).'@example.test';
     }
 
@@ -57,37 +42,16 @@ class VendorFactory extends Factory
     {
         $values = VendorType::values();
 
-        try {
-            if ($this->faker !== null) {
-                return $this->faker->randomElement($values);
-            }
-        } catch (Throwable) {
-        }
-
         return $values[array_rand($values)];
     }
 
     private function generateCity(): string
     {
-        try {
-            if ($this->faker !== null) {
-                return $this->faker->city();
-            }
-        } catch (Throwable) {
-        }
-
         return 'City '.Str::upper(Str::random(4));
     }
 
     private function generateDescription(): string
     {
-        try {
-            if ($this->faker !== null) {
-                return $this->faker->sentence();
-            }
-        } catch (Throwable) {
-        }
-
         return 'Sample vendor description.';
     }
 }
